37c7cbef9e45ced0a5b4f1dc9e918d1af091efa6
Luke Edwards <lukeedwards2022@gmail.com>
2024-07-29 13:20:25 -0500

    yeah

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,7 +19,29 @@ function renderList(todos) {
     let toDoList = document.getElementById('to-do-list')
     toDoList.innerHTML = ''
 
-    for (let do of todos) {
-        
+    for (let to of todos) {
+        if (to.isComplete === false) {
+            toDoList.innerHTML += `
+            <tr data-testid="toDoItem>
+                <td>${to.text}$</td>
+                <td>
+                    <button data-testid="completeButton" onclick="markTaskComplete(${todo.id})">Complete</button>
+                </td>`
+        }
+
+
     }
+}
+
+function addTodos() {
+    event.preventDefault()
+    let todosText = document.getElementById("todoText").value 
+
+    axios({
+        method: "POST",
+        url: "/todos",
+        data: {}
+    })
+    .then
+
 }
