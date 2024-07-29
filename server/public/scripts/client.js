1a6ea40612415108fba7938ce1e068e8581a7a71
Luke Edwards <lukeedwards2022@gmail.com>
2024-07-28 22:32:38 -0500

    get routes and rending on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,25 @@
 console.log('JS is sourced!');
+
+getToDO()
+
+function getToDO() {
+    axios({
+        method: 'GET',
+        url: '/todos'
+    })
+    .then((response) => {
+        renderList(response.data)
+    })
+    .catch((error) => {
+        console.log('ERROR', error)
+    })
+}
+
+function renderList(todos) {
+    let toDoList = document.getElementById('to-do-list')
+    toDoList.innerHTML = ''
+
+    for (let do of todos) {
+        
+    }
+}
